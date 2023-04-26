dc4d7c7d177b2509d2978f87e3c2fe03ba3211b8
Deepak Kumar <dk119819@gmail.com>
2023-04-27 01:38:55 +0530

    Commit

---
+++ Q_5_print_inputs_till_the_user_enters_x.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Q_5_print_inputs_till_the_user_enters_x {
+
+    //Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        System.out.print("Enter a number (or 'x' to exit): ");
+        while (true) {
+            String input = scan.nextLine();
+            if (input.equals("x")) {
+                break;
+            }
+            int num = Integer.parseInt(input);
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
