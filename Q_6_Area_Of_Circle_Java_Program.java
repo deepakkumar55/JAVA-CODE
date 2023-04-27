1f76cef5c69c0f433a25623ed7a97843e0fb8243
Deepak Kumar <dk119819@gmail.com>
2023-04-27 20:38:02 +0530

    .

--- Q_6_Area_Of_Circle_Java_Program.java
+++ Q_6_Area_Of_Circle_Java_Program.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Q_6_Area_Of_Circle_Java_Program {
 
     //Java program to calculate or to print area of a circle in a simple method
-
+    // A = π*r*r
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the radius:");
