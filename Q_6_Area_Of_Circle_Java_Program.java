7bcab771621c96ddb13d7c8610b9167d44ed183f
Deepak Kumar <dk119819@gmail.com>
2023-04-27 08:48:57 +0530

    .

---
+++ Q_6_Area_Of_Circle_Java_Program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q_6_Area_Of_Circle_Java_Program {
+
+    //Java program to calculate or to print area of a circle in a simple method
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius:");
+        double r= sc.nextDouble();
+        double  area=(22*r*r)/7 ;
+        System.out.println("Area of Circle is: " + area);
+    }
+}
