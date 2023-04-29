ace78f4c73d106514d4c1798c75191d8d6f01dc1
Deepak Kumar <dk119819@gmail.com>
2023-04-29 20:51:47 +0530

    .

--- Q_11_Palindrome_Program_2.java
+++ Q_11_Palindrome_Program_2.java
@@ -10,7 +10,7 @@ public class Q_11_Palindrome_Program_2 {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a string/number to check if it is a palindrome");
         original = in.nextLine();
-        int length = original.length();
+        int length = original.length(); 
         for ( int i = length - 1; i >= 0; i-- )
             reverse = reverse + original.charAt(i);
         if (original.equals(reverse))
