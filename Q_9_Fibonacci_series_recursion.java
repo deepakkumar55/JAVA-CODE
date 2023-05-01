a8bba087a8b66bd05dcab36a461b63c2ea564c67
Deepak Kumar <dk119819@gmail.com>
2023-05-01 22:06:22 +0530

    .

--- Q_9_Fibonacci_series_recursion.java
+++ Q_9_Fibonacci_series_recursion.java
@@ -7,7 +7,7 @@ public class Q_9_Fibonacci_series_recursion {
 
     public static void main(String[] args) {
         int n1=0,n2=1,n3,i,count=11;
-        System.out.print(n1+" "+n2);
+        System.out.print(n1+","+n2);
 
         for(i=2;i<count;++i)
         {
