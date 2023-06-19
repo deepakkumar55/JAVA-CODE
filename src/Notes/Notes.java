feb788a0c64fdb73c3c22d2b9e46ab6e2dd91aa2
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-06-19 12:50:59 +0530

    .

---
+++ src/Notes/Notes.java
@@ -0,0 +1,7 @@
+//DS:- Data Structures, ek prakar ka organizational system hai jisme hum data ko organize aur manage
+//        karne ke liye istemaal karte hain. Ismein hum data ko alag-alag tarike se store aur access
+//        kar sakte hain taki hum usse aasaani se istemaal kar sake.
+//    ex:- Jaise ki socho tum apne kapde ek almirah mein rakhte ho.
+//            Almirah mein kapdon ko organize karne ke liye tum alag-alag compartments bana sakte ho,
+//            jismein shirts, jeans, aur doosre kapde alag-alag sections mein rakhe jate hain. Isi tarah,
+//            data structures mein bhi hum alag-alag tarike se data ko organize karte hain.
