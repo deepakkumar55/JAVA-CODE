86669ac88981f66d195e7b6d703ee3b2b8cd4d5d
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-07-31 05:21:33 +0530

    .

--- src/Day1/Q_7.java
+++ src/Day1/Q_7.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
 
 public class Q_7 {
-    // Q 7. 
+    // Q 7. Find the Greatest Number Among Three Numbers in Java
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print(" ");
+        System.out.print("Enter The First Number:- ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter The Second Number:- ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter The third Number:- ");
+        int num3 = sc.nextInt();
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println(num1 + " is the greatest");
+        }
+
+        else if (num2 >= num1 && num2 >= num3) {
+            System.out.println(num2 + " is the greatest");
+        }
+
+        else if (num3 >= num1 && num3 >= num2) {
+            System.out.println(num3 + " is the greatest");
+        }
+
     }
 }
