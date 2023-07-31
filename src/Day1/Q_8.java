b63ec24cec361c01243a1d29bd94382a6500ba36
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-07-31 05:42:26 +0530

    .

--- src/Day1/Q_8.java
+++ src/Day1/Q_8.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Q_8 {
-    
+    // Q 8. Java Program to Check Leap Year or not
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int yr = sc.nextInt();
+        if(yr%4==0){
+            System.out.println("It's Leep Year");
+        }
+        else{
+            System.out.println("Not A leep Year.");
+        }
+    }
 }
---
+++ src/Day1/Q_9.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q_9 {
+    Q 9. Prime Number Program in Java
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+    }
+    
+}
