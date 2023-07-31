2bfd07457ca53d13208f6c90721dbb4f3779bbc5
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-07-31 06:11:52 +0530

    Create Q_11.java

---
+++ src/Day1/Q_11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q_11 {
+    // Q 11. Sum of digits of a Number in Java
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=sc.nextInt();
+        int sum=0;
+        while(num!=0){
+            sum += num % 10;
+            num = num / 10;
+        }
+        System.out.println ("Sum of digits : " + sum);
+    }
+    
+}
